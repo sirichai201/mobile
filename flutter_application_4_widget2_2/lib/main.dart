515bf92cdb722bcfd97c222ad42de40939bb3e1a
sirichai201 <sirichaichantharasri4@gmail.com>
2023-02-10 20:33:05 +0700

    tttt

--- flutter_application_4_widget2_2/lib/main.dart
+++ flutter_application_4_widget2_2/lib/main.dart
@@ -34,67 +34,80 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool isLoading = false;
   bool isAnimating = true;
-  //ทำต่อถึงบรรทัด40
+  ButtonState state = ButtonState.init;
   @override
-  Widget build(BuildContext context) => /*isLoading */ false
-      ? LoadingPage()
-      : Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-          ),
-          backgroundColor: Colors.black,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      textStyle: TextStyle(fontSize: 24),
-                      minimumSize: Size.fromHeight(72),
-                      shape: StadiumBorder(),
-                    ),
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      await Future.delayed(Duration(seconds: 3));
-                      setState(() {
-                        isLoading = false;
-                      });
-                    },
-                    child: isLoading
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              CircularProgressIndicator(color: Colors.white),
-                              SizedBox(width: 24),
-                              Text('Please wait....'),
-                            ],
-                          )
-                        : const Text('Login')),
-                /*NeonButton(Colors.red),
-                NeonButton(Colors.greenAccent.shade700),
-                NeonButton(Colors.blueAccent.shade700),
-                NeonButton(Colors.yellowAccent.shade700),
-                NeonButton(Colors.blue),
-                NeonButton(Colors.white),*/
-                /*SpinKitCircle(
-              size: 140,
-              itemBuilder: (context, index) {
-                final colors = [
-                  Colors.green,
-                  Colors.redAccent.shade700,
-                  Colors.yellowAccent.shade400
-                ];
-                final Color = colors[index % colors.length];
-                return DecoratedBox(decoration: BoxDecoration(color: Color));
-              },
-            )*/
-              ],
+  Widget build(BuildContext context) {
+    /*isLoading */
+    //?LoadingPage()
+    final Width = MediaQuery.of(context).size.width;
+    final isDone = state == ButtonState.done;
+    bool isStretched = isAnimating || state == ButtonState.init;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(32),
+          child:AnimatedContainer(
+            
+          )
+        child: Center(
+            /*child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 24),
+                        minimumSize: Size.fromHeight(72),
+                        shape: StadiumBorder(),
+                      ),
+                      onPressed: () async {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        await Future.delayed(Duration(seconds: 3));
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                      child: isLoading
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                CircularProgressIndicator(color: Colors.white),
+                                SizedBox(width: 24),
+                                Text('Please wait....'),
+                              ],
+                            )
+                          : const Text('Login')),
+                  /*NeonButton(Colors.red),
+                  NeonButton(Colors.greenAccent.shade700),
+                  NeonButton(Colors.blueAccent.shade700),
+                  NeonButton(Colors.yellowAccent.shade700),
+                  NeonButton(Colors.blue),
+                  NeonButton(Colors.white),*/
+                  /*SpinKitCircle(
+                size: 140,
+                itemBuilder: (context, index) {
+                  final colors = [
+                    Colors.green,
+                    Colors.redAccent.shade700,
+                    Colors.yellowAccent.shade400
+                  ];
+                  final Color = colors[index % colors.length];
+                  return DecoratedBox(decoration: BoxDecoration(color: Color));
+                },
+              )*/
+                ],
+              ),
+            ),*/
             ),
-          ),
-        );
-  /* Listener(
+      ),
+    );
+    /* Listener(
               onPointerDown: (event) => setState(() {
                 isPressed = true;
               }),
@@ -146,7 +159,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
     */
-
+  }
 }
 
 class LoadingPage extends StatelessWidget {
