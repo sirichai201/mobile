4076dd410fbd569346c0c2ee922fffc42bced27a
sirichai201 <sirichaichantharasri4@gmail.com>
2023-02-28 20:59:03 +0700

    lab7_3

--- flutter_application_7_3/lib/main.dart
+++ flutter_application_7_3/lib/main.dart
@@ -2,10 +2,9 @@
 // ignore_for_file: override_on_non_overriding_member
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_7/favorites_page.dart';
 import 'package:flutter_application_7/model/transaction.dart';
 import 'package:flutter_application_7/providers/transaction_provider.dart';
-import 'package:flutter_application_7/screens/form_screen.dart';
+import 'package:flutter_application_7/screens/home_screen.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -45,153 +44,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  void initState() {
-    super.initState();
-    Provider.of<TransactionProvider>(context, listen: false).initData();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return FormScreen();
-                  }));
-                },
-                icon: Icon(Icons.add))
-          ],
-        ),
-        /*bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.deepOrange,
-          items: <Widget>[
-            Icon(Icons.add, size: 30),
-            Icon(Icons.list, size: 30),
-            Icon(Icons.compare_arrows, size: 30),
-          ],
-          onTap: (index) {
-            //Handle button tap
-          },
-        ),
-        drawer: NavigationDrawer(),*/
-        body: Consumer(builder: (context, TransactionProvider provider, child) {
-          var count = provider.transactions.length; //นับจำนวนข้อมูล
-          if (count <= 0) {
-            return Center(
-                child: Text(
-              "ไม่พบข้อมูล",
-              style: TextStyle(fontSize: 35),
-            ));
-          } else {
-            return ListView.builder(
-                itemCount: provider.transactions.length,
-                itemBuilder: (context, int index) {
-                  Transactions data = provider.transactions[index];
-                  return Card(
-                    elevation: 5,
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: FittedBox(
-                          child: Text(data.amount.toString()),
-                        ),
-                      ),
-                      title: Text(data.title),
-                      subtitle: Text(
-                          DateFormat("dd/MM/yyyy HH:mm:ss").format(data.date)),
-                    ),
-                  );
-                });
-          }
-        }));
+    return HomeScreen();
   }
-}
-
-class NavigationDrawer extends StatelessWidget {
-  const NavigationDrawer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            buildHeader(context),
-            buildMenuItems(context),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildHeader(BuildContext context) => Container(
-        color: Colors.black38,
-        padding: EdgeInsets.only(
-          top: 24 + MediaQuery.of(context).padding.top,
-          bottom: 24,
-        ),
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 52,
-              backgroundImage: NetworkImage('src/classroom1'),
-            ),
-            SizedBox(height: 12),
-            Text('sirichai',
-                style: TextStyle(fontSize: 28, color: Colors.black)),
-            Text('Sirichaia@Dev.com',
-                style: TextStyle(fontSize: 28, color: Colors.white)),
-          ],
-        ),
-      );
-  Widget buildMenuItems(BuildContext context) => Container(
-      padding: const EdgeInsets.all(24),
-      child: Wrap(
-        runSpacing: 16,
-        children: [
-          ListTile(
-            leading: const Icon(Icons.home_outlined),
-            title: const Text('Home'),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const MyHomePage(title: 'Home Page')));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.favorite_border),
-            title: const Text('Favourites'),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const FavouritesPage()));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.workspaces_outline),
-            title: const Text('Workflow'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.update),
-            title: const Text('Updates'),
-            onTap: () {},
-          ),
-          const Divider(color: Colors.black54),
-          ListTile(
-            leading: const Icon(Icons.account_tree_outlined),
-            title: const Text('Plugins'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.notifications_outlined),
-            title: const Text('Notifications'),
-            onTap: () {},
-          ),
-        ],
-      ));
 }
---
+++ flutter_application_7_3/lib/screens/home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_7/model/transaction.dart';
+import 'package:flutter_application_7/providers/transaction_provider.dart';
+import 'package:flutter_application_7/screens/form_screen.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<TransactionProvider>(context, listen: false).initData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("แอพรายจ่าย"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  var Navigation;
+                  Navigation.push(context,
+                      MaterialPageRoute(builder: (context) {
+                    return FormScreen();
+                  }));
+                },
+                icon: Icon(Icons.add)),
+            IconButton(
+                onPressed: () {
+                  SystemNavigator.pop();
+                },
+                icon: Icon(Icons.exit_to_app, color: Colors.red))
+          ],
+        ),
+        body: Consumer(builder: (context, TransactionProvider provider, child) {
+          var count = provider.transactions.length; //นับจำนวนข้อมูล
+          if (count <= 0) {
+            return const Center(
+                child: Text(
+              "ไม่พบข้อมูล",
+              style: TextStyle(fontSize: 35),
+            ));
+          } else {
+            return ListView.builder(
+                itemCount: provider.transactions.length,
+                itemBuilder: (context, int index) {
+                  Transactions data = provider.transactions[index];
+                  return Card(
+                    elevation: 5,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: FittedBox(
+                          child: Text(data.amount.toString()),
+                        ),
+                      ),
+                      title: Text(data.title),
+                      subtitle: Text(
+                          DateFormat("dd/MM/yyyy HH:mm:ss").format(data.date)),
+                    ),
+                  );
+                });
+          }
+        }));
+  }
+}
