c699219d83489c567b1f86ae5f1bc68756a0f23e
sirichai201 <sirichaichantharasri4@gmail.com>
2023-03-10 18:20:51 +0700

    เพิ่มรู)หน้า welcome

--- flutter_application_10_version01/lib/screen/welcome.dart
+++ flutter_application_10_version01/lib/screen/welcome.dart
@@ -5,31 +5,37 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 class WelcomeScreen extends StatelessWidget {
-
   final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text("ยินดีต้อนรับ"),),
+      appBar: AppBar(
+        title: Text("ยินดีต้อนรับ"),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Center(
           child: Column(
             children: [
-                Text(auth.currentUser.email,style: TextStyle(fontSize: 25),),
-                ElevatedButton(
-                  child: Text("ออกจากระบบ"),
-                  onPressed: (){
-                     auth.signOut().then((value){
-                        Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context){
-                            return HomeScreen();
-                        }));
-                     });
-                  }, 
-                )
-            ],),
+              Image.asset("assets/images/dog.png"),
+              Text(
+                auth.currentUser.email,
+                style: TextStyle(fontSize: 25),
+              ),
+              ElevatedButton(
+                child: Text("ออกจากระบบ"),
+                onPressed: () {
+                  auth.signOut().then((value) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) {
+                      return HomeScreen();
+                    }));
+                  });
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
