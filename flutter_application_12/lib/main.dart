e8c6bcc2e517f59dcdc19efc890ae59eb879fa0b
sirichai201 <sirichaichantharasri4@gmail.com>
2023-03-24 14:29:01 +0700

    double

--- flutter_application_12/lib/main.dart
+++ flutter_application_12/lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<ExchangeRate> getExchangeRate() async {
     var url = "https://open.er-api.com/v6/latest/AMD";
     var response = await http.get(Uri.parse(url));
-    //แปลง JSON เป็น Object
+    // Convert JSON to object
     _dataFromAPI = exchangeRateFromJson(response.body);
     return _dataFromAPI;
   }
@@ -45,12 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('อัตราแลกเปลี่ยนสกุลเงิน'),
+          title: Text('Exchange Rates'),
         ),
         body: FutureBuilder(
           future: getExchangeRate(),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            //ตรวจสอบสถานะ กรณ๊ดึงข้อมูลสำเร็จ
             if (snapshot.connectionState == ConnectionState.done) {
               var result = snapshot.data;
               double money = 100;
@@ -58,21 +57,40 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: const EdgeInsets.all(10.0),
                 child: Column(
                   children: [
-                    MoneyBox("AMD", money, Colors.green, 100),
+                    MoneyBox(
+                      "AMD",
+                      money,
+                      Colors.green,
+                      100,
+                    ),
                     SizedBox(height: 5),
                     MoneyBox(
-                        "USD", money * result.rates["USD"], Colors.red, 100),
+                      "USD",
+                      double.parse(
+                          (money * result.rates["USD"]).toStringAsFixed(2)),
+                      Colors.red,
+                      100,
+                    ),
                     SizedBox(height: 5),
                     MoneyBox(
-                        "JPY", money * result.rates["JPY"], Colors.orange, 100),
+                      "JPY",
+                      double.parse(
+                          (money * result.rates["JPY"]).toStringAsFixed(2)),
+                      Colors.orange,
+                      100,
+                    ),
                     SizedBox(height: 5),
                     MoneyBox(
-                        "THB", money * result.rates["THB"], Colors.purple, 100),
+                      "THB",
+                      double.parse(
+                          (money * result.rates["THB"]).toStringAsFixed(2)),
+                      Colors.purple,
+                      100,
+                    ),
                   ],
                 ),
               );
             }
-            //กรณ๊อยู่ระหว่างดึงข้อมูลให้แสดง progress bar
             return LinearProgressIndicator();
           },
         ));
